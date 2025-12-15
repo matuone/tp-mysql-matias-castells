d0e737a93db9e7a99dcc961554662dab7a47ab02
matiascastells <matiascastells@gmail.com>
2025-12-15 20:24:49 -0300

    Ejercicio 10 - JOIN multiple con historial clinico

--- sql/06_joins.sql
+++ sql/06_joins.sql
@@ -6,3 +6,18 @@ SELECT
   CONCAT(d.nombre, ' ', d.apellido) AS dueno
 FROM mascotas m
 INNER JOIN duenos d ON m.id_dueno = d.id;
+
+/* Ejercicio 10 - JOIN múltiple con historial clínico */
+
+SELECT
+  m.nombre AS nombre_mascota,
+  m.especie,
+  CONCAT(d.nombre, ' ', d.apellido) AS dueno,
+  CONCAT(v.nombre, ' ', v.apellido) AS veterinario,
+  hc.fecha_registro,
+  hc.descripcion
+FROM historial_clinico hc
+INNER JOIN mascotas m ON hc.id_mascota = m.id
+INNER JOIN duenos d ON m.id_dueno = d.id
+INNER JOIN veterinarios v ON hc.id_veterinario = v.id
+ORDER BY hc.fecha_registro DESC;
