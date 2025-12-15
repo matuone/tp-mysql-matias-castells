1516ce0a40c2b46672098afc178a64025253d90b
matiascastells <matiascastells@gmail.com>
2025-12-15 20:31:12 -0300

    arreglando comentario que estaba mal explicado en ejercicio 6

--- sql/03_inserts.sql
+++ sql/03_inserts.sql
@@ -4,7 +4,7 @@ INSERT INTO duenos (nombre, apellido, telefono, direccion) VALUES
 ('Diego', 'Maradona', '1010-1986', 'Segurola y Habana 4310 7A'),
 ('Carlos', 'Bilardo', '1986-0000', 'Estadio Azteca');
 
-/* Ejercicio 6 - Insertando tabla veterinarios primero antes que las mascotas, ya que mascotas depende de esta tabla */
+/* Ejercicio 6 - Insertando veterinarios antes de tablas dependientes */
 
 INSERT INTO veterinarios (nombre, apellido, matricula, especialidad) VALUES
 ('Guillermo', 'Barros Schelotto', 'MAT-001', 'Clinica General'),
