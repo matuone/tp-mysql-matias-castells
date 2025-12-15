e28623dd38aab047edbe512c5ec0701b782dae21
matiascastells <matiascastells@gmail.com>
2025-12-15 19:55:02 -0300

    ejercicio 6 insertando mascotas

--- sql/03_inserts.sql
+++ sql/03_inserts.sql
@@ -9,3 +9,10 @@ INSERT INTO duenos (nombre, apellido, telefono, direccion) VALUES
 INSERT INTO veterinarios (nombre, apellido, matricula, especialidad) VALUES
 ('Guillermo', 'Barros Schelotto', 'MAT-001', 'Clinica General'),
 ('Martin', 'Palermo', 'MAT-002', 'Cirugia');
+
+/* Ejercicio 6 - Insertando mascotas */ 
+INSERT INTO mascotas (nombre, especie, fecha_nacimiento, id_dueno) VALUES
+('Gimenez', 'Perro', '2018-06-15', 1),
+('La Tombi', 'Gato', '2020-03-10', 2),
+('Figal', 'Perro', '2017-11-01', 3);
+
