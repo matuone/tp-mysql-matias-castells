60f7caed60157a4bd5e49b46e5b4217ed6ec6b82
matiascastells <matiascastells@gmail.com>
2025-12-15 19:41:49 -0300

    ejercicio 6 insertando 3 personas en la tabla duenos

---
+++ sql/03_inserts.sql
@@ -0,0 +1,5 @@
+/* Ejercicio 6 - Inserts tabla duenos */
+INSERT INTO duenos (nombre, apellido, telefono, direccion) VALUES
+('Roman', 'Riquelme', '1010-1212', 'Brandsen 805'),
+('Diego', 'Maradona', '1010-1986', 'Segurola y Habana 4310 7A'),
+('Carlos', 'Bilardo', '1986-0000', 'Estadio Azteca');
