969cd4c669fb7e8b14868ad83a3ff4d67a556bc3
matiascastells <matiascastells@gmail.com>
2025-12-15 19:49:58 -0300

    Realizando ejercicio 6, primero insertando tabla de veterinarios

--- sql/03_inserts.sql
+++ sql/03_inserts.sql
@@ -3,3 +3,9 @@ INSERT INTO duenos (nombre, apellido, telefono, direccion) VALUES
 ('Roman', 'Riquelme', '1010-1212', 'Brandsen 805'),
 ('Diego', 'Maradona', '1010-1986', 'Segurola y Habana 4310 7A'),
 ('Carlos', 'Bilardo', '1986-0000', 'Estadio Azteca');
+
+/* Ejercicio 6 - Insertando tabla veterinarios primero antes que las mascotas, ya que mascotas depende de esta tabla */
+
+INSERT INTO veterinarios (nombre, apellido, matricula, especialidad) VALUES
+('Guillermo', 'Barros Schelotto', 'MAT-001', 'Clinica General'),
+('Martin', 'Palermo', 'MAT-002', 'Cirugia');
