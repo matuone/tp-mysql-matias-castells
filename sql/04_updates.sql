906162d5ba49c91644799eb9d694722c87abf97d
matiascastells <matiascastells@gmail.com>
2025-12-15 20:05:35 -0300

    update de especialidad de un veterinario

--- sql/04_updates.sql
+++ sql/04_updates.sql
@@ -3,3 +3,9 @@
 UPDATE duenos
 SET direccion = 'Av. Libertador 1234'
 WHERE id = 1;
+
+/* Ejercicio 7 - Update especialidad de un veterinario */
+
+UPDATE veterinarios
+SET especialidad = 'Traumatologia'
+WHERE matricula = 'MAT-002';
